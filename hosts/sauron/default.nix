6d9db538e52cbaaeb546433cc421d040deb725cf
heph <srht@mrkeebs.eu>
2025-04-25 20:28:57 +0200

    enable jellyfin on sauron and intel gpu driver
    also add borgbackup package

--- hosts/sauron/default.nix
+++ hosts/sauron/default.nix
@@ -1,5 +1,10 @@
 { config, lib, pkgs, ... }:
 
+let
+  hostname = "sauron";
+  localDomain = hostname + ".hephnet.lan";
+in
+
 {
   imports = [ # Include the results of the hardware scan.
     ./hardware-configuration.nix
@@ -13,7 +18,7 @@
   # Use the systemd-boot EFI boot loader.
   boot.loader.grub.efiSupport = true;
 
-  networking.hostName = "sauron"; # Define your hostname.
+  networking.hostName = hostname; # Define your hostname.
   boot.loader.grub.enable = true; # Enables wireless support via wpa_suppli
   boot.loader.grub.efiInstallAsRemovable = true; # Easiest to use and most
   boot.loader.grub.device = "nodev";
@@ -21,11 +26,33 @@
   i18n.defaultLocale = "en_US.UTF-8";
   networking.hostId = "a854bd58";
 
+  hardware.graphics = {
+    enable = true;
+    extraPackages = with pkgs; [
+      vpl-gpu-rt
+    ];
+  };
+
+  services.zfs.autoScrub.enable = true;
+  services.jellyfin = {
+    enable = true;
+    dataDir = "/media";
+  };
+
+  services.nginx = {
+    enable = true;
+    recommendedProxySettings = true;
+    virtualHosts."jelly.${localDomain}" = {
+      proxyPass = "http://127.0.0.1:8096";
+      proxyWebSockets = true;
+    };
+  };
+
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILCmIz2Selg5eJ77lvpJHgDJiRIOZbucMjDK5zrhTEWK"
   ];
 
-  environment.systemPackages = with pkgs; [ vim mg wget ];
+  environment.systemPackages = with pkgs; [ vim mg wget borgbackup ];
 
   services.openssh.enable = true;
 
@@ -33,4 +60,3 @@
   networking.firewall.enable = true;
 
 }
-
