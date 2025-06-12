7ddf553ce1b092b4e188b5bcbdfbe8ac72d64c3b
heph <srht@mrkeebs.eu>
2025-06-12 14:37:55 +0200

    add claude-code derivation with latest version
    upstream is currently 1.0.17, and the Pro plan isnt' supported yet.
    Gonna manually update it to the last available version

---
+++ pkgs/cc/package.nix
@@ -0,0 +1,46 @@
+{
+  lib,
+  buildNpmPackage,
+  fetchzip,
+  nodejs_20,
+}:
+
+buildNpmPackage rec {
+  pname = "claude-code";
+  version = "1.0.21";
+
+  nodejs = nodejs_20; # required for sandboxed Nix builds on Darwin
+
+  src = fetchzip {
+    url = "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-${version}.tgz";
+    hash = "sha256-CtNY7CduAg/QWs58jFnJ/3CMRpRKrJzD49Gqw7kSsao=";
+  };
+
+  npmDepsHash = "sha256-102mZ67C1hPK1NPLV9zCc8toQTZb+nHlq14Dqes5pIw=";
+
+  postPatch = ''
+    cp ${./package-lock.json} package-lock.json
+  '';
+
+  dontNpmBuild = true;
+
+  AUTHORIZED = "1";
+
+  # `claude-code` tries to auto-update by default, this disables that functionality.
+  # https://docs.anthropic.com/en/docs/agents-and-tools/claude-code/overview#environment-variables
+  postInstall = ''
+    wrapProgram $out/bin/claude \
+      --set DISABLE_AUTOUPDATER 1
+  '';
+
+  passthru.updateScript = ./update.sh;
+
+  meta = {
+    description = "Agentic coding tool that lives in your terminal, understands your codebase, and helps you code faster";
+    homepage = "https://github.com/anthropics/claude-code";
+    downloadPage = "https://www.npmjs.com/package/@anthropic-ai/claude-code";
+    license = lib.licenses.unfree;
+    maintainers = [ lib.maintainers.malo ];
+    mainProgram = "claude";
+  };
+}
