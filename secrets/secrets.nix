c42aebd3957a480a1607a82cb2333ad18b14717c
heph <srht@mrkeebs.eu>
2025-10-19 18:37:16 +0200

    agenix config file

---
+++ secrets/secrets.nix
@@ -0,0 +1,18 @@
+let
+  heph = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO5082w5a8Dljon2Qs9N3eGd5+Tg52zzawlZGeHw8D12";
+  freya = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILCmIz2Selg5eJ77lvpJHgDJiRIOZbucMjDK5zrhTEWK";
+  users = [
+    freya
+    heph
+  ];
+in
+{
+  "wg-key-freya.age".publicKeys = [
+    freya
+    heph
+  ];
+  "foo.age".publicKeys = [
+    heph
+    freya
+  ];
+}
