e5efa4a41dd5e01ed7d64e7e9fce445a1ee2f048
heph <srht@mrkeebs.eu>
2025-11-16 20:04:16 +0100

    add agenix

--- dev.nix
+++ dev.nix
@@ -1,18 +1,27 @@
 {
-  perSystem = { pkgs, self', inputs', ... }: {
-    formatter = pkgs.nixpkgs-fmt;
-    devShells.default = with pkgs;
-      mkShell {
-        buildInputs = [
-          sops
-          just
-          ssh-to-age
-          age
-          nixos-rebuild
-        ];
-        shellHook = ''
-          export SOPS_AGE_KEY_FILE=$(pwd)/secrets/age-privkey.txt
-        '';
-      };
-  };
+  perSystem =
+    {
+      pkgs,
+      self',
+      inputs',
+      ...
+    }:
+    {
+      formatter = pkgs.nixpkgs-fmt;
+      devShells.default =
+        with pkgs;
+        mkShell {
+          buildInputs = [
+            sops
+            just
+            ssh-to-age
+            age
+            ragenix
+            nixos-rebuild
+          ];
+          shellHook = ''
+            export SOPS_AGE_KEY_FILE=$(pwd)/secrets/age-privkey.txt
+          '';
+        };
+    };
 }
