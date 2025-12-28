392068ed5f72883f4191ae68a43bbf0edf95a6f8
heph <srht@mrkeebs.eu>
2025-12-28 16:18:12 +0100

    nix fmt

--- hosts/hermes/configuration.nix
+++ hosts/hermes/configuration.nix
@@ -1,4 +1,5 @@
-{ pkgs, inputs, ... }: {
+{ pkgs, inputs, ... }:
+{
   flake.nixosConfigurations.hermes = inputs.nixpkgs.lib.nixosSystem {
     system = "x86_64-linux";
     modules = [
@@ -7,9 +8,12 @@
       { nixpkgs.config.allowUnfree = true; }
       inputs.simple-nixos-mailserver.nixosModule
       ../../modules/common/default.nix
-      ({ modulesPath, ... }: {
-        imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
-      })
+      (
+        { modulesPath, ... }:
+        {
+          imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
+        }
+      )
     ];
   };
 }
