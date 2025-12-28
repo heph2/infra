d9f542323934386b396fe1728e134af5330a896f
heph <srht@mrkeebs.eu>
2025-12-28 16:18:23 +0100

    add some secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,6 +2,7 @@ let
   heph = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO5082w5a8Dljon2Qs9N3eGd5+Tg52zzawlZGeHw8D12";
   freya = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILCmIz2Selg5eJ77lvpJHgDJiRIOZbucMjDK5zrhTEWK";
   zima = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKmSgm97MHi+1dwh79BpyePwiXAcD2R+ceUKbBn6pVRv";
+  sauron = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKECQ6GB+aeG76Sx8Ht4JJH0JmRrMNsf/uoA42eDZFK0";
   users = [
     freya
     heph
@@ -20,5 +21,17 @@ in
     heph
     freya
     zima
+  ];
+  "cloudflare_api_token.age".publicKeys = [
+    heph
+    freya
+    zima
+    sauron
+  ];
+  "paperless-oidc-client-secret.age".publicKeys = [
+    heph
+    freya
+    zima
+    sauron
   ];
 }
