920e0bd89d0b197245a2f18c43794d5f0f9db045
heph <srht@mrkeebs.eu>
2025-12-28 16:18:39 +0100

    deploy paperless-ngx

--- hosts/sauron/paperless.nix
+++ hosts/sauron/paperless.nix
@@ -1,8 +1,12 @@
-{ config, pkgs, ... }:
-
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 {
   age.secrets.paperless_oidc_secret = {
-    file = "../../paperless-oidc-client-secret.age";
+    file = ../../secrets/paperless-oidc-client-secret.age;
     mode = "640";
     owner = "paperless";
     group = "paperless";
@@ -10,29 +14,10 @@
 
   services.paperless = {
     enable = true;
+    environmentFile = config.age.secrets.paperless_oidc_secret.path;
     settings = {
       PAPERLESS_URL = "https://paperless.pochi.casa";
-
-      PAPERLESS_APPS = "allauth.socialaccount.providers.openid_connect";
-
-      PAPERLESS_SOCIALACCOUNT_PROVIDERS = {
-        openid_connect = {
-          APPS = [
-            {
-              provider_id = "pocketid";
-              name = "Pocket ID";
-              client_id = "a76dd172-aabe-4366-942d-b56fd7704ae5";
-              secret = config.age.secrets.paperless_oidc_secret.path;
-              settings = {
-                server_url = "https://auth.pochi.casa/.well-known/openid-configuration";
-              };
-            }
-          ];
-        };
-      };
-
-      PAPERLESS_DISABLE_REGULAR_LOGIN = true;
-      PAPERLESS_REDIRECT_LOGIN_TO_SSO = true;
+      PAPERLESS_SOCIAL_AUTO_SIGNUP = true;
     };
   };
 }
