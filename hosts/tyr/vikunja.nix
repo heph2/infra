487ad4d33c9e8013159e0bf1faa34bbbd37acf72
heph <srht@mrkeebs.eu>
2026-02-12 14:07:53 +0100

    set everything from secrets and envs

--- hosts/tyr/vikunja.nix
+++ hosts/tyr/vikunja.nix
@@ -1,8 +1,5 @@
 {
   config,
-  pkgs,
-  lib,
-  inputs,
   ...
 }:
 let
@@ -15,33 +12,32 @@ in
     frontendHostname = domain;
     frontendScheme = "https";
     environmentFiles = [ config.age.secrets.vikunja_oidc_secret.path ];
-    settings = {
-      auth = {
-        local = {
-          enabled = false;
-        };
-        openid = {
-          enabled = true;
-          redirecturl = "https://${domain}/auth/openid/pocketid";
-          providers = [
-            {
-              name = "Pocket-ID";
-              authurl = issuer;
-              logouturl = "${issuer}/api/oidc/end-session/";
-              clientid = "436a474a-15aa-444a-8ba1-e7dce352eb9d";
-              scope = "openid profile email";
-            }
-          ];
-        };
-      };
-    };
+    settings = { };
+  };
+
+  # OIDC config via environment (secret comes from environmentFiles)
+  systemd.services.vikunja.environment = {
+    VIKUNJA_AUTH_LOCAL_ENABLED = "false";
+    VIKUNJA_AUTH_OPENID_ENABLED = "true";
+    VIKUNJA_AUTH_OPENID_REDIRECTURL = "https://${domain}/auth/openid/pocketid";
+    VIKUNJA_AUTH_OPENID_PROVIDERS_POCKETID_NAME = "Pocket-ID";
+    VIKUNJA_AUTH_OPENID_PROVIDERS_POCKETID_AUTHURL = issuer;
+    VIKUNJA_AUTH_OPENID_PROVIDERS_POCKETID_LOGOUTURL = "${issuer}/api/oidc/end-session/";
+    VIKUNJA_AUTH_OPENID_PROVIDERS_POCKETID_CLIENTID = "436a474a-15aa-444a-8ba1-e7dce352eb9d";
+    VIKUNJA_AUTH_OPENID_PROVIDERS_POCKETID_SCOPE = "openid profile email";
   };
 
   age.secrets.vikunja_oidc_secret = {
     file = ../../secrets/vikunja-oidc-client-secret.age;
     path = "/run/agenix/vikunja-oidc.env";
-    mode = "640";
-    owner = "vikunja";
-    group = "vikunja";
   };
+
+  services.caddy.virtualHosts."${domain}".extraConfig = ''
+    encode zstd gzip
+    tls {
+      dns cloudflare {env.CLOUDFLARE_API_TOKEN}
+      resolvers 1.1.1.1 8.8.8.8
+    }
+    reverse_proxy 127.0.0.1:3456
+  '';
 }
