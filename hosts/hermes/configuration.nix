5756a832efb351935c2cf3a1d7d884c4ce30ba81
heph <srht@mrkeebs.eu>
2026-02-24 11:25:33 +0100

    add agenix module

--- hosts/hermes/configuration.nix
+++ hosts/hermes/configuration.nix
@@ -1,19 +1,16 @@
-{ pkgs, inputs, ... }:
-{
+{ pkgs, inputs, ... }: {
   flake.nixosConfigurations.hermes = inputs.nixpkgs.lib.nixosSystem {
     system = "x86_64-linux";
     modules = [
       ./default.nix
       inputs.sops-nix.nixosModules.sops
+      inputs.agenix.nixosModules.default
       { nixpkgs.config.allowUnfree = true; }
       inputs.simple-nixos-mailserver.nixosModule
       ../../modules/common/default.nix
-      (
-        { modulesPath, ... }:
-        {
-          imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
-        }
-      )
+      ({ modulesPath, ... }: {
+        imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
+      })
     ];
   };
 }
