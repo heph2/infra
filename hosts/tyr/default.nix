4d3076cd3a17453912148d8a05906eca71405d69
heph <srht@mrkeebs.eu>
2026-03-05 15:05:33 +0100

    nixfmt

--- hosts/tyr/default.nix
+++ hosts/tyr/default.nix
@@ -10,23 +10,20 @@
     ./hardware-configuration.nix
     ./homebox.nix
     ./vikunja.nix
+    ./grafana.nix
   ];
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  services.tailscale = {
-    enable = true;
-  };
+  services.tailscale = { enable = true; };
 
   # IPV6 Configuration
   networking.interfaces.enp0s31f6 = {
-    ipv6.addresses = [
-      {
-        address = "2a07:7e81:85f5::babe";
-        prefixLength = 64;
-      }
-    ];
+    ipv6.addresses = [{
+      address = "2a07:7e81:85f5::babe";
+      prefixLength = 64;
+    }];
   };
   networking.defaultGateway6 = {
     address = "fe80::6f4:1cff:fe18:162";
@@ -47,10 +44,7 @@
     mongodbPackage = pkgs.mongodb;
   };
 
-  environment.systemPackages = with pkgs; [
-    vim
-    wget
-  ];
+  environment.systemPackages = with pkgs; [ vim wget ];
 
   services.openssh.enable = true;
   networking.firewall.allowedTCPPorts = [
@@ -68,37 +62,23 @@
   ];
   networking.wireguard.interfaces = {
     wg0 = {
-      ips = [
-        "10.1.1.5/32"
-        "2a0f:85c1:c4d:1234::5/128"
-      ];
+      ips = [ "10.1.1.5/32" "2a0f:85c1:c4d:1234::5/128" ];
       listenPort = 51820;
       privateKeyFile = "/root/vellutata.wg0.priv";
 
-      peers = [
-        {
-          publicKey = "JE3KvXkupkYkM3eJ2mSmSeBCAZvHBqv7k4XZ/WSUc1w=";
-          allowedIPs = [
-            "0.0.0.0/0"
-            "::/0"
-          ];
-          endpoint = "193.57.159.213:51820";
-          persistentKeepalive = 25;
-        }
-      ];
+      peers = [{
+        publicKey = "JE3KvXkupkYkM3eJ2mSmSeBCAZvHBqv7k4XZ/WSUc1w=";
+        allowedIPs = [ "0.0.0.0/0" "::/0" ];
+        endpoint = "193.57.159.213:51820";
+        persistentKeepalive = 25;
+      }];
     };
   };
 
   services.prometheus.exporters.node = {
     enable = true;
     port = 9000;
-    enabledCollectors = [
-      "ethtool"
-      "softirqs"
-      "systemd"
-      "tcpstat"
-      "wifi"
-    ];
+    enabledCollectors = [ "ethtool" "softirqs" "systemd" "tcpstat" "wifi" ];
   };
   services.prometheus = {
     enable = true;
@@ -106,19 +86,23 @@
     scrapeConfigs = [
       {
         job_name = "node";
-        static_configs = [
-          {
-            targets = [ "localhost:${toString config.services.prometheus.exporters.node.port}" ];
-          }
-        ];
+        static_configs = [{
+          targets = [
+            "localhost:${
+              toString config.services.prometheus.exporters.node.port
+            }"
+          ];
+        }];
       }
       {
         job_name = "dovecot";
-        static_configs = [
-          {
-            targets = [ "hermes:${toString config.services.prometheus.exporters.dovecot.port}" ];
-          }
-        ];
+        static_configs = [{
+          targets = [
+            "hermes:${
+              toString config.services.prometheus.exporters.dovecot.port
+            }"
+          ];
+        }];
       }
     ];
   };
