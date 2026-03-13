8e85a298d8447ba00da321755e16b4919c6c084c
heph <srht@mrkeebs.eu>
2026-03-13 15:22:23 +0100

    try to overlay without success on pixel6

--- hosts/pixie/configuration.nix
+++ hosts/pixie/configuration.nix
@@ -3,9 +3,14 @@
     system = "aarch64-linux";
     modules = [
       ./default.nix
-      {
-        nixpkgs.config.allowUnfree = true;
-      }
+      { nixpkgs.config.allowUnfree = true; }
+      ({ pkgs, ... }: {
+        nixpkgs.overlays = [
+          (final: prev: {
+            ttyd = prev.ttyd.overrideAttrs (old: { patches = [ ]; });
+          })
+        ];
+      })
       ({ modulesPath, ... }: {
         imports = [
           (modulesPath + "/installer/scan/not-detected.nix")
