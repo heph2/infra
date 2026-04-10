c8fd5ad54cfbf64ebf356d95c59ec67ac6750f1b
heph <srht@mrkeebs.eu>
2026-04-10 13:43:42 +0200

    add cachix for apple silicon

--- hosts/fenrir/default.nix
+++ hosts/fenrir/default.nix
@@ -51,11 +51,13 @@ in {
         "https://noctalia.cachix.org"
         "https://cache.nixos.org"
         "https://nix-community.cachix.org"
+        "https://nixos-apple-silicon.cachix.org"
       ];
       extra-trusted-public-keys = [
         "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "nixos-apple-silicon.cachix.org-1:8psDu5SA5dAD7qA0zMy5UT292TxeEPzIz8VVEr2Js20="
       ];
     };
   };
