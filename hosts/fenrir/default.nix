4ee3d13f890b21824d79bd4498948b9aa0f86b42
heph <srht@mrkeebs.eu>
2026-06-12 11:59:47 +0200

    add numtide cache

--- hosts/fenrir/default.nix
+++ hosts/fenrir/default.nix
@@ -94,12 +94,14 @@ in
         "https://cache.nixos.org"
         "https://nix-community.cachix.org"
         "https://nixos-apple-silicon.cachix.org"
+        "https://cache.numtide.com"
       ];
       extra-trusted-public-keys = [
         "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "nixos-apple-silicon.cachix.org-1:8psDu5SA5dAD7qA0zMy5UT292TxeEPzIz8VVEr2Js20="
+        "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
       ];
     };
   };
