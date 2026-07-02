7ff8a6ed335de40441c265f30aa76066e8fa08e5
heph <srht@mrkeebs.eu>
2026-07-02 11:53:23 +0200

    add cuppy host and update sha for caddy

--- hosts/sauron/caddy.nix
+++ hosts/sauron/caddy.nix
@@ -34,6 +34,10 @@ let
       host = "jellyseerr.pochi.casa";
       upstream = "localhost:5055";
     }
+    {
+      host = "cuppy.pochi.casa";
+      upstream = "192.168.0.104:30298 192.168.0.105:30298";
+    }
   ];
 
   caddyFile = pkgs.writeText "Caddyfile" (''
@@ -97,7 +101,7 @@ in {
   services.caddy.configFile = caddyFile;
   services.caddy.package = pkgs.caddy.withPlugins {
     plugins = [ "github.com/caddy-dns/cloudflare@v0.2.2" ];
-    hash = "sha256-dnhEjopeA0UiI+XVYHYpsjcEI6Y1Hacbi28hVKYQURg=";
+    hash = "sha256-7DGnojZvcQBZ6LEjT0e5O9gZgsvEeHlQP9aKaJIs/Zg=";
   };
   networking.firewall.allowedTCPPorts = [ 80 443 ];
   age.secrets.cloudflare = {
