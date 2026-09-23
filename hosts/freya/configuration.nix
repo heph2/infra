5e3c5a135628735fc901e66a694f0b3b2e64f254
heph <srht@mrkeebs.eu>
2026-09-23 13:48:22 +0200

    remove from freya

--- hosts/freya/configuration.nix
+++ hosts/freya/configuration.nix
@@ -27,7 +27,6 @@ in
       inputs.trcc_gif.nixosModules.trcc-gif
       inputs.handy.nixosModules.default
       config.infra.modules.nixos.home-manager
-      # config.infra.modules.nixos.comfyui
       config.infra.modules.nixos.plakar-backup
       config.infra.modules.nixos.plakar-bitwarden-backup
       config.infra.modules.nixos.plakar-routeros-backup
